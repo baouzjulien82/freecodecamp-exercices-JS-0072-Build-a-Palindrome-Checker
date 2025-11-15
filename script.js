0a325ea37e953d4b6ba1064a996cfae2d2782b6b
Julien Baouz <baouzjulien@gmail.com>
2025-11-15 11:45:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ const button = document.getElementById('check-btn');
 const input = document.getElementById('text-input');
 const result = document.getElementById('result');
 
-// Alert si entrée vide
+// regEx
+const regEx = /qqc/;
+
+// Fonction isPalindrome
+const isPalindrome = (value) => {
+  regEx.test(value);
+};
+
+// Event Listener
 button.addEventListener('click', () => {
-  if(input.textContent === "") {
+  if(input.value === "") {
     alert('Please input a value')
   };
+  result.textContent = isPalindrome(input.value) ? `${input.value} is a palindrome` : `${input.value} is not a palindrome`;
+  input.value = "";
 });
