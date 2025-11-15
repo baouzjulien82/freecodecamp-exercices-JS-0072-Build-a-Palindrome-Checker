7e0b91b6ff864c55cc81ba29c62b4e003bb6ae15
Julien Baouz <baouzjulien@gmail.com>
2025-11-15 11:12:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// déclarations des variables
+const button = document.getElementById('check-btn');
+const input = document.getElementById('text-input');
+const result = document.getElementById('result');
 
+// Alert si entrée vide
+button.addEventListener('click', () => {
+  if(input.textContent === "") {
+    alert('Please input a value')
+  };
+});
